8085bdd2242343505bb4e2d581497f69c4da5635
Daniel Harrison <daniel.harrison.lima@gmail.com>
2024-06-29 19:50:18 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.ts
@@ -0,0 +1,38 @@
+import { PostgresHelper } from "../../db/postgres/helper"
+
+export interface UserInterfaceUpdate {
+  firstName?: string;
+  lastName?: string,
+  email?: string;
+  password?: string;
+}
+
+type UserKeys = keyof UserInterfaceUpdate;
+
+export class PostgresUpdateUserRepository {
+  async execute(userId : string, updateUserParams : UserInterfaceUpdate) {
+    const updateFields : string[] = []
+    const updateValues = []
+    
+    Object.keys(updateUserParams).forEach((key) => {
+      const typedKey = key as UserKeys;
+      updateFields.push(`${typedKey} = $${updateValues.length + 1}`);
+      updateValues.push(updateUserParams[typedKey]);
+    });
+
+    updateValues.push(userId)
+
+    const updateQuery = `
+      UPDATE users
+      SET ${updateFields.join(', ')}
+      WHERE id = $${updateValues.length}
+      RETURNING *
+    `
+    const updateUser = await PostgresHelper.query(
+      updateQuery,
+      updateValues
+    )
+
+    return updateUser[0]
+  }
+}
